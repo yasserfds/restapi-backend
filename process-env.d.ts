2b0dc924ca97cd59e35283f9cd78d6375274155f
TheGiantNinja <yasserfedsi34@gmail.com>
2023-11-07 23:35:58 +0100

    Deleted all files

--- process-env.d.ts
+++
@@ -1,9 +0,0 @@
-declare global {
-  namespace restAPI {
-    interface ProcessEnv {
-      PORT: string;
-      MONGODB_URI: string;
-      // add more environment variables and their types here
-    }
-  }
-}

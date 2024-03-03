f90b468f697c864a10be6674240fb9289756856a
TheGiantNinja <yasserfedsi34@gmail.com>
2024-03-03 17:00:06 +0100

    Done with the second part of code

--- src/author/author.router.ts
+++ src/author/author.router.ts
@@ -3,7 +3,6 @@ import type { Request, Response } from "express";
 import { body, validationResult } from "express-validator";
 
 import * as AuthorService from "./author.service";
-import { request } from "http";
 
 export const authorRouter = express.Router();
 
@@ -16,3 +15,38 @@ authorRouter.get("/", async (request: Request, response: Response) => {
     return response.status(500).json(error.message);
   }
 });
+
+// GET: A single author by ID
+authorRouter.get("/:id", async (request: Request, response: Response) => {
+  const id: number = parseInt(request.params.id, 10);
+  try {
+    const author = await AuthorService.getAuthor(id);
+    if (author) {
+      return response.status(200).json(author);
+    }
+    return response.status(404).json("Author could not be found");
+  } catch (error: any) {
+    return response.status(500).json(error.message);
+  }
+});
+
+// POST: Create an Author
+// Params: firstName, lastName
+authorRouter.post(
+  "/",
+  body("firstName").isString(),
+  body("lastName").isString(),
+  async (request: Request, response: Response) => {
+    const errors = validationResult(request);
+    if (!errors.isEmpty()) {
+      return response.status(400).json({ errors: errors.array() });
+    }
+    try {
+      const author = request.body;
+      const newAuthor = await AuthorService.createAuthor(author);
+      return response.status(201).json(newAuthor);
+    } catch (error: any) {
+      return response.status(500).json(error.message);
+    }
+  }
+);

10dbc725da6286564c6cecb8aafc7470ff257704
TheGiantNinja <yasserfedsi34@gmail.com>
2024-03-08 21:03:15 +0100

    Done

--- src/book/book.router.ts
+++ src/book/book.router.ts
@@ -3,7 +3,6 @@ import type { Request, Response } from "express";
 import { body, validationResult } from "express-validator";
 
 import * as BookService from "./book.service";
-import { request } from "http";
 
 export const bookRouter = express.Router();
 
@@ -13,6 +12,79 @@ bookRouter.get("/", async (request: Request, response: Response) => {
     const books = await BookService.listBooks();
     return response.status(200).json(books);
   } catch (error: any) {
-    return response.status(500).json(error.messgae);
+    return response.status(500).json(error.message);
+  }
+});
+
+// GET: A book based on the id
+bookRouter.get("/:id", async (request: Request, response: Response) => {
+  const id: number = parseInt(request.params.id, 10);
+
+  try {
+    const book = await BookService.getBooks(id);
+    if (book) {
+      return response.status(200).json(book);
+    }
+  } catch (error: any) {
+    return response.status(500).json;
+  }
+});
+
+// POST: Add a book
+bookRouter.post(
+  "/",
+  body("title").isString(),
+  body("authorId").isInt(),
+  body("datePublished").isDate().toDate(),
+  body("isFiction").isBoolean(),
+  async (request: Request, response: Response) => {
+    const errors = validationResult(request);
+    if (!errors.isEmpty()) {
+      return response.status(400).json({ errors: errors.array() });
+    }
+    try {
+      const book = request.body;
+      const newBook = await BookService.createBook(book);
+      return response.status(201).json(newBook);
+    } catch (error: any) {
+      return response.status(500).json(error.message);
+    }
+  }
+);
+
+// PUT: Update book
+bookRouter.put(
+  "/",
+  body("title").isString(),
+  body("authorId").isInt(),
+  body("datePublished").isDate().toDate(),
+  body("isFiction").isBoolean(),
+  async (request: Request, response: Response) => {
+    const errors = validationResult(request);
+    if (!errors.isEmpty()) {
+      return response.status(400).json({ errors: errors.array() });
+    }
+
+    const id: number = parseInt(request.params.id, 10);
+
+    try {
+      const book = request.body;
+      const updateBook = await BookService.updateBook(book, id);
+      return response.status(201).json(updateBook);
+    } catch (error: any) {
+      return response.status(500).json(error.message);
+    }
+  }
+);
+
+// Delete a book
+bookRouter.delete("/:id", async (request: Request, response: Response) => {
+  const id: number = parseInt(request.params.id, 10);
+
+  try {
+    await BookService.deleteBook(id);
+    return response.status(204).json("Book was successfully deleted");
+  } catch (error: any) {
+    return response.status(500).json(error.message);
   }
 });
